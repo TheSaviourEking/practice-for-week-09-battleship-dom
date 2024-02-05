1873b8e6ffd03a083ffeef50d6d243b2e4b9fde2
TheSaviourEking <saviour.eking@outlook.com>
2024-02-05 05:27:25 +0100

    [chore]: tired, would call it a day

--- assets/js/index.js
+++ assets/js/index.js
@@ -21,40 +21,12 @@ document.addEventListener('DOMContentLoaded', () => {
             gameDiv.appendChild(cell)
         }
     }
+    console.log(gameDiv)
     document.body.appendChild(gameDiv);
-
-    //     gameDiv.addEventListener('click', event => {
-    //         const row = event.target.dataset.row;
-    //         const col = event.target.dataset.col;
-    //         // if (row && col) {
-    //         //     const hit = board.makeHit(row, col);
-    //         //     if (!hit) {
-    //         //         event.target.classList.add('miss');
-    //         //     } else {
-    //         //         event.target.innerText = hit;
-    //         //         event.target.classList.add('hit');
-    //         //     }
-    //         // }
-
-    //         if (!board.isGameOver()) {
-    //             if (row && col) {
-    //                 const hit = board.makeHit(row, col);
-    //                 if (!hit) {
-    //                     event.target.classList.add('miss');
-    //                 } else {
-    //                     event.target.innerText = hit;
-    //                     event.target.classList.add('hit');
-    //                 }
-    //             }
-    //         } else {
-    //             ga
-    //         }
-    //     })
-    // })
 
     gameDiv.addEventListener('click', gameDivClick)
 
-    function gameDivClick(e) {
+    function gameDivClick(event) {
         const row = event.target.dataset.row;
         const col = event.target.dataset.col;
         if (!board.isGameOver()) {
@@ -70,10 +42,46 @@ document.addEventListener('DOMContentLoaded', () => {
         } else {
             const h1 = document.getElementsByTagName('h1')[0];
             const winText = document.createElement('p');
+            winText.setAttribute('id', 'winText');
             winText.innerText = 'YOU WIN!';
 
             h1.after(winText);
+            setTimeout(() => {
+                document.body.removeChild(winText);
+
+                // setTimeout()
+                createButton(h1);
+            }, 3000, h1)
             gameDiv.removeEventListener('click', gameDivClick)
         }
     }
+
+    function createButton(h1) {
+        const button = document.createElement('button');
+        button.innerText = 'Reset Game';
+
+        h1.after(button)
+
+        button.addEventListener('click', event => {
+            event.preventDefault();
+
+            // board.populateGrid();
+            board = new Board();
+
+            resetBoard(board);
+        })
+    }
 })
+
+function resetBoard() {
+    const gameDiv = document.getElementById('game-container')
+    // for (let row = 0; row < 9; row++) {
+    //     for (let col = 0; col < 9; col++) {
+
+    //     }
+    // }
+    for (const child of gameDiv.children) {
+        child.innerText = '';
+        child.classList.remove('miss', 'hit');
+    }
+}
