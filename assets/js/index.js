b80665bb92f6038e46724264f6bcbb211b6a40d7
TheSaviourEking <saviour.eking@outlook.com>
2024-02-06 19:45:06 +0100

    [chore]: implement and refactor requirements for phase 2

--- assets/js/index.js
+++ assets/js/index.js
@@ -23,28 +23,73 @@ window.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    // use eventd-delegation to delegate the cell click events to the container
-    grid.addEventListener('click', event => {
+    // use event-delegation to delegate the cell click events to the container
+    // grid.addEventListener('click', event => {
+    //     if (!board.isGameOver()) {
+    //         const row = event.target.dataset.row;
+    //         const col = event.target.dataset.col;
+
+    //         if (row && col) {
+    //             const guess = board.makeHit(row, col);
+    //             if (guess && typeof guess === 'number') {
+    //                 // get and add guess value to page
+    //                 event.target.innerText = guess;
+    //                 event.target.classList.add('hit');
+    //                 console.log(event.target.textContent)
+    //             } else {
+    //                 event.target.classList.add('miss');
+    //             }
+    //         }
+    //     } else {
+    //         const h1 = document.getElementsByTagName('h1')[0];
+    //         const p = document.createElement('p');
+    //         p.innerText = 'YOU WIN!!';
+    //         h1.after(p)
+    //         setTimeout(() => {
+    //             document.body.removeChild(p);
+    //         }, 2000)
+    //     }
+    // })
+
+    grid.addEventListener('click', handleClick)
+
+    document.body.appendChild(grid);
+
+    // click handler
+    function handleClick(event) {
         const row = event.target.dataset.row;
         const col = event.target.dataset.col;
 
+
         if (row && col) {
             const guess = board.makeHit(row, col);
-            if (guess && typeof guess === 'number') {
-                // get and add guess value to page
-                event.target.innerText = guess;
-                event.target.classList.add('hit');
-                console.log(event.target.textContent)
+            console.log('GameOVER?', board.isGameOver())
+            if (!board.isGameOver()) {
+                if (guess && typeof guess === 'number') {
+                    // get and add guess value to page
+                    event.target.innerText = guess;
+                    event.target.classList.add('hit');
+                    console.log(event.target.textContent)
+                } else {
+                    event.target.classList.add('miss');
+                }
             } else {
-                event.target.classList.add('miss');
+                // display YOU WIN text
+                const text = document.createElement('p');
+                text.innerText = 'YOU WIN!';
+
+                const h1 = document.getElementsByTagName('h1')[0];
+                setTimeout(() => {
+                    document.body.removeChild(text);
+                }, 3000);
+                h1.after(text);
+
+                const cells = document.getElementsByClassName('game-container')[0].childNodes;
+                cells.forEach(cell => cell.classList.add('disable'));
+
+                event.currentTarget.classList.add('disable');
+                grid.removeEventListener('click', handleClick);
             }
         }
-
-        // console.log(row, col)
-        // const hit = board.makeHit(row, col);
-    })
-
-    document.body.appendChild(grid);
+    }
 });
-
-// function 
